49f03eee2ed1ea2ea5cde68faddfc170fa6e8200
unknown <arciozg@gmail.com>
2015-12-28 18:56:25 +0100

    Probably works

--- dane2.m
+++ dane2.m
@@ -2,16 +2,18 @@ b=8;
 Ea=230000;
 Eb=230000;
 Ec=230000;
-Cp=(Cp+b)*e-9;
-Cg=(0.6+0.01*b);
+Cp=(0.25+b)*1e-9;
+Cg=(0.6+0.01*b)*1e-6;
 Rl=6.5;
 Ll=0.150 - b/1000;
 Ro=500+20*b;
 Lo=0.5-0.01*b;
+Rz=0.0000000010;
+omega=2*50*pi;
 
 
 galezie=[
-%ng--wp--wk----R_Ohm----L_H-----C_F----E_kV---fi_st
+%ng--wp--wk----R_Ohm----L_H-----C_F----E_V---fi_st
   1   0   1     1e-6       0     1e9     Ea      0  %napiecie fazowe UA    
   2   0   2     1e-6       0     1e9     Eb   -120  %napiecie fazowe UB  
   3   0   3     1e-6       0     1e9     Ec    120  %napiecie fazowe UC  
@@ -32,7 +34,7 @@ galezie=[
  17   0   5        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L2 0
  18   0   4        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L1 0  
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- 29   0   4        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L1 0 
+ 19   0   4        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L1 0 
  20   0   5        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L2 0   
  21   0   6        0       0      Cg      0    0    %pojemnoœæ faza ziemia  L3 0 
  22   4   5        0       0      Cp      0    0    %pojemnoœæ miêdzy fazowa L1 L2   
@@ -53,5 +55,5 @@ galezie=[
  35   0   8       Ro      Lo     1e9       0    0    %obci¹¿enie w fazie L2
  36   0   9       Ro      Lo     1e9       0    0    %obci¹¿enie w fazie L3
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+ 37   0   5       Rz       0     1e9       0    0
  ];
--- projekt.m
+++ projekt.m
@@ -1,23 +1,132 @@
-daneetk;
+dane2;
 
-ng = galezie(:,1);
-wp = galezie(:,2);
-wk = galezie(:,3);
-R = galezie(:,4);
-L = galezie(:,5);
-C = galezie(:,6);
-E = galezie(:,7);
-fi = galezie(:,8);
+% ng = galezie(:,1);
+% wp = galezie(:,2);
+% wk = galezie(:,3);
+% R = galezie(:,4);
+% L = galezie(:,5);
+% C = galezie(:,6);
+% E = galezie(:,7);
+% fi = galezie(:,8);
+% file = fopen('wynik.txt','w');
 
-f=50;
-X = 2*pi*f.*L;
+Yz=zeros(9,9);
 
-file = fopen('wynik.txt','w');
-
-disp('ng--wp--wk----R_Ohm---L_mH---C_nF--E_kV--fi_st');
-for k=1:length(ng),
-fprintf('%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
-fprintf(file,'%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
+for m=1:size(galezie)
+    
+    beg = galezie(m,2);
+    kon = galezie(m,3);
+    
+    if beg==0
+        continue;
+    end;
+    
+    if galezie(m,6) > 1e-11
+         Yz(beg,kon) = -1/(galezie(m,4)+1i*omega*galezie(m,5)+(1/(1i*omega*galezie(m,6))));
+    else Yz(beg,kon) = -1/(galezie(m,4)+1i*omega*galezie(m,5));
+    end;
+    
+    for k=1:size(galezie)
+        if (galezie(k,2)) == beg && (galezie(k,3)) == kon
+            if(galezie(k,6) > 1e-11)
+                 Yz(beg,kon) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5)+(1/(1i*omega*galezie(k,6))));
+            else Yz(beg,kon) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5));
+            end;
+        end;
+    end;
 end;
 
-fclose(file);
+for m=1:size(galezie)
+    
+    kon = galezie(m,2);
+    beg = galezie(m,3);
+    
+    if kon==0
+        continue;
+    end;
+    
+    if galezie(m,6) ~= 0
+         Yz(beg,kon) = -1/(galezie(m,4)+1i*omega*galezie(m,5)+(1/(1i*omega*galezie(m,6))));
+    else Yz(beg,kon) = -1/(galezie(m,4)+1i*omega*galezie(m,5));
+    end;
+    
+    for k=1:size(galezie)
+        if galezie(k,2) == beg && galezie(k,3) == kon
+            if(galezie(k,6) ~= 0)
+                 Yz(beg,kon) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5)+(1/(1i*omega*galezie(k,6))));
+            else Yz(beg,kon) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5));
+            end;
+        end;
+    end;
+end;
+
+
+
+%Przekatne Yz
+for m=1:9
+     
+    for k=1:size(galezie)
+        if galezie(k,2) == m || galezie(k,3) == m
+            if galezie(k,6) ~= 0
+                 Yz(m,m) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5)+(1/(1i*omega*galezie(k,6))));
+            else Yz(m,m) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5));
+            end;
+        end;
+    end;
+    
+end
+
+Izw=zeros(9,1);
+
+Izw(1,1)=-Ea*Yz(1,1);
+Izw(2,1)=-Eb*(cos(-120)+1i*sin(-120))*Yz(2,2);
+Izw(3,1)=-Ec*(cos(120)+1i*sin(120))*Yz(3,3);
+V=Yz\Izw;
+%Macierz Ezr
+Ezr=zeros(37,1);
+Ezr(1,1)=Ea;
+Ezr(2,1)=Eb*(cos(-120)+1i*sin(-120));
+Ezr(3,1)=Ec*(cos(120)+1i*sin(120));
+
+%Macierz Ug
+Ug=zeros(37,1);
+for m=1:size(galezie)
+     beg = galezie(m,2);
+     kon = galezie(m,3);
+    for k=1:size(galezie)
+        if galezie(k,2)== beg && galezie(k,3)==kon && galezie(k,2)~=0
+            Ug(m)=V(galezie(k,3))-V(galezie(k,2));
+        end
+         if galezie(k,2)==beg && galezie(k,3)==kon && galezie(k,2)==0
+            Ug(m)=V(galezie(k,3));
+        end
+    end
+end
+
+Ug(1)=-Ezr(1);
+Ug(2)=-Ezr(2);
+Ug(3)=-Ezr(3);
+
+
+%Macierz Ydiag
+Ydiag=zeros(37,37);
+for k=1:size(galezie)
+            if galezie(k,6) ~= 0
+                 Ydiag(k,k) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5)+(1/(1i*omega*galezie(k,6))));
+            else Ydiag(k,k) =+ -1/(galezie(k,4)+1i*omega*galezie(k,5));
+            end;
+end;
+R=galezie(:,4);
+L=galezie(:,5)*10^-3; Xl=omega*L;
+C=galezie(:,6)*10^-9; Xc=1./(omega*C);
+Z=R+1i*(Xl-Xc); %impedancje poszczególnych ga³êzi
+
+Ig=Ug./Z;
+
+% disp('ng--wp--wk----R_Ohm---L_mH---C_nF--E_kV--fi_st');
+% for k=1:length(ng),
+% fprintf('%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
+% fprintf(file,'%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
+% end;
+% 
+% fclose(file);
