88c860cd5505d5374016ed83540d4eb742f70462
unknown <arciozg@gmail.com>
2016-01-11 22:34:05 +0100

    Final version.

--- projekt.m
+++ projekt.m
@@ -1,61 +1,108 @@
 dane2;
-Yz=zrob_mat(galezie);
-
-%Macierz pradow zwarciowych
-Izw=zeros(9,1);
-Izw(1,1)=-Ea*Yz(1,1);
-Izw(2,1)=-Eb*(cos(-120)+1i*sin(-120))*Yz(2,2);
-Izw(3,1)=-Ec*(cos(120)+1i*sin(120))*Yz(3,3);
-
-%Macierz potencjalow wezlowych
-V=Yz\Izw;
+Yz = zrob_mat(galezie);
 
 %Macierz Ezr
 Ezr=zeros(37,1);
-Ezr(1,1)=Ea;
-Ezr(2,1)=Eb*(cos(-120)+1i*sin(-120));
-Ezr(3,1)=Ec*(cos(120)+1i*sin(120));
+Ezr(1,1) = Ea;
+Ezr(2,1) = Eb*(cos(-pi*2/3)+1i*sin(-pi*2/3));
+Ezr(3,1) = Ec*(cos(pi*2/3)+1i*sin(pi*2/3));
+
+%Macierz pradow zwarciowych
+Izw = zeros(9,1);
+Izw(1,1) = Ezr(1,1)*Yz(1,1);
+Izw(2,1) = Ezr(2,1)*Yz(2,2);
+Izw(3,1) = Ezr(3,1)*Yz(3,3);
+
+%Macierz potencjalow wezlowych
+V = Yz^(-1);
+V = V * Izw;
 
 %Macierz Ug
-Ug=zeros(37,1);
+Ug = zeros(37,1);
 for m=1:size(galezie)
-     beg = galezie(m,2);
-     kon = galezie(m,3);
-    for k=1:size(galezie)
-        if galezie(k,2)== beg && galezie(k,3)==kon && galezie(k,2)~=0
-            Ug(m)=V(galezie(k,3))-V(galezie(k,2));
-        end;
-         if galezie(k,2)==beg && galezie(k,3)==kon && galezie(k,2)==0
-            Ug(m)=V(galezie(k,3));
-         end;
+    Ug(m,1) = V(galezie(m,3));
+    if galezie(m,2)~=0
+         Ug(m,1) = Ug(m,1) - V(galezie(m,2));
     end;
 end;
 
-%Dodaje napiecia zrodlowe do galezi
-Ug(1)=Ug(1)+Ezr(1);
-Ug(2)=Ug(2)+Ezr(2);
-Ug(3)=Ug(3)+Ezr(3);
 
-%Macierz Ydiag
-Ydiag=zeros(37,37);
-for k=1:size(galezie)           
-    Ydiag(k,k) = Ydiag(k,k) + 1/(galezie(k,4)+1i*(omega*galezie(k,5)-(1/(omega*galezie(k,6)))));
+%Macierz admitancji ga³êziowych
+R = galezie(:,4);
+L = galezie(:,5); 
+C = galezie(:,6);
+Xc = zeros(37,1);
+Xl = zeros(37,1);
+Z = zeros(37,1);
+Y = zeros(37,1);
+
+for k=1:size(C)
+    Xc(k,1) = 1/(omega * C(k,1));
+    Xl(k,1) = omega * L(k,1);
+    Z(k,1) = R(k,1) + 1i * (Xl(k,1) - Xc(k,1));
+    Y(k,1) = 1/Z(k,1);
 end;
 
-%Druga metoda
-R=galezie(:,4);
-L=galezie(:,5); Xl=omega*L;
-C=galezie(:,6); Xc=1./(omega*C);
-Z=R+1i*(Xl-Xc); 
-
 %Prady galeziowe
-Ig = Ug./Z;
-Ig2 = Ydiag*Ug;
+Ig = zeros(37,1);
+for k=1:37
+    Ig(k,1) = (Ug(k,1) - Ezr(k,1))*Y(k,1);
+end;
 
-% disp('ng--wp--wk----R_Ohm---L_mH---C_nF--E_kV--fi_st');
-% for k=1:length(ng),
-% fprintf('%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
-% fprintf(file,'%2.0f %4.2f %4.2f %4.2e %4.2f %4.2e %4.2f %4.2f\n',ng(k),wp(k),wk(k),R(k),L(k),C(k),E(k),fi(k));
-% end;
-% 
-% fclose(file);
+ for i=1:3
+     Ig(i,1) = 0;
+     for j=3:size(galezie)
+         if galezie(j,2) == i || galezie(j,3) == i
+             Ig(i,1) = Ig(i,1) + Ig(j);
+         end
+     end
+ end
+
+%Macierz fi
+fi = radtodeg(angle(Ug));
+
+%Pgen;
+for k=1:3
+Pgen(k) = real(Ezr(k) * conj(Ig(k)));
+end;
+for k=4:36
+    Pgen(k) = 0;
+end;
+
+%Qgen;
+for k=1:3
+Qgen(k) = imag(Ezr(k) * conj(Ig(k)));
+end;
+
+for k=4:36
+    Qgen(k) = 0;
+end;
+
+%Podb;
+for k=1:3
+    Podb(k) = 0;
+end;
+
+for k=4:36
+    Podb(k) = real(Ug(k) * conj(Ig(k)));
+end;
+%Qgen;
+for k=1:3
+    Qodb(k) = 0;
+end;
+
+for k=4:36
+    Qodb(k) = imag(Ug(k) * conj(Ig(k)));
+end;
+%Gen_fi
+genfi = 0;%radtodeg(angle(Pgen))-radtodeg(angle(Qgen));
+
+
+file = fopen('wyniki.txt','w');
+ 
+fprintf(file,'nr_gal--wp--wk---U [kV] --- fi---I [kA]--gen fi--gen P--gen Q--odb P -- odb Q\n');
+for k=1:36
+    fprintf(file,'%2d %7d %3d  %4.2e  %4.0f  %4.2e %4.2f  %4.0f  %7.2f %7.2f %7.2f \n',k,galezie(k,2),galezie(k,3),abs(Ug(k)/1000),fi(k),abs(Ig(k))/1000,genfi,Pgen(k)/1000000,Qgen(k)/1000000,Podb(k)/1000000,Qodb(k)/1000000);
+end;
+
+fclose(file);
--- zrob_mat.m
+++ zrob_mat.m
@@ -1,51 +1,34 @@
 function [Yz] = zrob_mat(galezie)
-dane2;
-Yz=zeros(9,9);
 
+dane2;
+
+%Macierz impedancji wzajemnych
+Yz=zeros(9,9);
 for m=1:size(galezie)
     
     beg = galezie(m,2);
     kon = galezie(m,3);
     
-    if beg==0
+    if beg == 0
         continue;
     end;
-    Yz(beg,kon) = -1/(galezie(m,4)+1i*(omega*galezie(m,5)-(1/(omega*galezie(m,6)))));
+    
+    Yz(beg,kon) = -1/(galezie(m,4) + 1i*(omega*galezie(m,5) - (1/(omega * galezie(m,6)))));
+    Yz(kon,beg) = -1/(galezie(m,4) + 1i*(omega*galezie(m,5) - (1/(omega * galezie(m,6)))));
     
     for k=1:size(galezie)
-        if (galezie(k,2)) == beg && (galezie(k,3)) == kon
-                 Yz(beg,kon) =Yz(beg,kon)+ -1/(galezie(k,4)+1i*(omega*galezie(k,5)-(1/(omega*galezie(k,6)))));
+        if galezie(k,2) == beg && galezie(k,3) == kon && m ~= galezie(k,1)
+            Yz(beg,kon) = Yz(beg,kon) - 1/(galezie(k,4) + 1i*(omega * galezie(k,5) - (1/(omega * galezie(k,6)))));
+            Yz(kon,beg) = Yz(kon,beg) - 1/(galezie(k,4) + 1i*(omega * galezie(k,5) - (1/(omega * galezie(k,6)))));
         end;
     end;
 end;
-
-for m=1:size(galezie)
-    
-    kon = galezie(m,2);
-    beg = galezie(m,3);
-    
-    if kon==0
-        continue;
-    end;
-
-         Yz(beg,kon) = -1/(galezie(m,4)+1i*(omega*galezie(m,5)-(1/(omega*galezie(m,6)))));
-
-    for k=1:size(galezie)
-        if galezie(k,2) == beg && galezie(k,3) == kon
-          
-            Yz(beg,kon) = Yz(beg,kon) + -1/(galezie(k,4)+1i*(omega*galezie(k,5)-(1/(omega*galezie(k,6)))));
-        end;
-    end;
-end;
-
-
 
 %Przekatne Yz
 for m=1:9
     for k=1:size(galezie)
         if galezie(k,2) == m || galezie(k,3) == m
-            Yz(m,m) =Yz(m,m)+ -1/(galezie(k,4)+1i*(omega*galezie(k,5)-(1/(omega*galezie(k,6)))));
+            Yz(m,m) = Yz(m,m) + 1/(galezie(k,4) + 1i*(omega*galezie(k,5) - (1/(omega * galezie(k,6)))));
         end;
     end;
-    
 end
